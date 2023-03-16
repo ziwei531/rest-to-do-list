d82a059ff2a1e39e2df40a44ea2734f72b44e860
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2023-03-16 19:18:01 +0800

    cors

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(function (req, res, next) {
 	if (allowedOrigins.indexOf(origin) > -1) {
 		res.setHeader("Access-Control-Allow-Origin", origin);
 	}
-	res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	res.header("Access-Control-Allow-Credentials", true);
 	return next();
